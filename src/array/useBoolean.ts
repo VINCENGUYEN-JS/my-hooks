02a2180d68558d9d5fb70af10c99f40bd21f8dfe
VinceNguyen <nguyenquangv95@gmail.com>
2021-12-26 20:06:50 +0700

    add-use-boolean-hook-and-write-test

---
+++ src/array/useBoolean.ts
@@ -0,0 +1,33 @@
+import React, { useState, useCallback, useMemo, SetStateAction } from 'react';
+
+type UseBooleanAction = (initial: boolean) => [boolean, UseBooleanActions];
+
+type UseBooleanActions = {
+  setValue: React.Dispatch<SetStateAction<boolean>>;
+  toggle: () => void;
+  setTrue: () => void;
+  setFalse: () => void;
+};
+
+export type UseBoolean = [boolean, UseBooleanActions];
+
+export const useBoolean: UseBooleanAction = (initial) => {
+  const [value, setValue] = useState<boolean>(initial);
+  const toggle = useCallback(() => setValue((v) => !v), []);
+  const setTrue = useCallback(() => setValue(true), []);
+  const setFalse = useCallback(() => setValue(false), []);
+
+  const actions = useMemo(
+    () => ({
+      setValue,
+      toggle,
+      setTrue,
+      setFalse,
+    }),
+    [],
+  );
+
+  return useMemo(() => [value, actions], [value, actions]);
+};
+
+export default useBoolean;
